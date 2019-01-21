2002be641c7e7d5f6020f3e1fc271b6b55e925c7
jacob <jacob_a_reed@yahoo.com>
2019-01-20 22:21:55 -0500

    added in a few react pages

---
+++ client/src/components/auction.js
@@ -0,0 +1,17 @@
+class Auction extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Auction Page</h1>
+        <ul>
+        <li>Website Visitors: 20,000</li>
+        <li>Money Earned: $800</li>
+        <li>Website Type: Clickbank Affiliate</li>
+        </ul>
+
+      </div>
+    );
+  }
+}
+
+export default Auction
---
+++ client/src/components/test-form.js
@@ -0,0 +1,13 @@
+class testForm extends Component {
+  render() {
+    return (
+      <form action="">
+        <h1>Sample Form</h1>
+        <input type='text' name='test' placeholder='Enter Text here...' /><br/>
+        <input type='submit' value='Submit'/>
+      </form>
+    );
+  }
+}
+
+export default testForm

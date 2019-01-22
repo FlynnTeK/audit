466949f99200b78c1208a4f244d583d129bd7c55
jacob <jacob_a_reed@yahoo.com>
2019-01-22 02:07:24 -0500

    removed captchas from main contract

--- contracts/Website.sol
+++ contracts/Website.sol
@@ -3,28 +3,19 @@ pragma solidity ^0.5.0;
 
 
 contract Website {
- 
+
  uint storedData;
 
 
 //Website - logs visitors, user clicks, and form submits; tracks who is the owner; receives public key and encrypted private key of encryption wallet
 
-struct Account 
-	{
-	string loginURL;
-	string username;
-	string hash;
-	}
-
-mapping (domain => Account) loginCredentials;
-string[] public accounts;
 
 string domain;
 address owner;
-string accounts
+string state;
 address encryptorPublickey;
 string encryptorPrivateKey;
-
+string captchaImageURL;
 
 constructor(address publicKey, string encryptedPrivateKey) public
 {
@@ -32,9 +23,9 @@ encryptorPublickey = publicKey;
 encryptorPrivateKey = encryptedPrivateKey;
 owner = msg.sender;
 }
-  
+
 function set(uint x) public {
-  
+
   storedData = x;
 
   }
@@ -47,5 +38,20 @@ function set(uint x) public {
 
   }
 
+event analytics (
+	string address,
+	string eventType,
+	string elementID
+);
+
+function logAction(string _address, string _eventType, string _elementID) public
+	{
+	analytics(_address, _eventType, _elementID);
+	}
+
+function websiteStats()
+  {
+
+  }
 
 }

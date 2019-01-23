78af1f2d64cec9c4e5456edee2d0a64377cd9c26
jacob <jacob_a_reed@yahoo.com>
2019-01-23 15:44:05 -0500

    added onto auction part of main contract

--- contracts/Website.sol
+++ contracts/Website.sol
@@ -67,17 +67,106 @@ function getDomain() public
     return owner;
     }
 
+  function getCaptchaImage() public
+    {
+    return captchaImageURL;
+    }
+
+  function getState() public
+    {
+    return state;
+    }
 //Auction - tracks highest bidder; transfers contract ownership; pays all parties involved their percentages
+uint auctionEnd;
+
+function startAuction(uint biddingTime) public
+  {
+  require(msg.sender === owner);
+  auctionEnd = now + biddingTime;
+  state = "auction";
+  }
+
 uint highestBid;
 address highestBidder;
+mapping(address => uint) pendingReturns;
 
-function placeBid () Payable
+
+function placeBid () public Payable
   {
     require(
-            msg.value > highestBid,
-            "There already is a higher bid."
-        );
-    highestBid = msg.amount;
-    highestBidder = msg.sender;
-  }
+        state === "auction",
+        "Not currently selling website"
+      );
+
+    require(
+               now <= auctionEnd,
+               "Auction already ended."
+           );
+
+           require(
+               msg.value > highestBid,
+               "There already is a higher bid."
+           );
+
+           if (highestBid != 0) {
+               pendingReturns[highestBidder] += highestBid;
+           }
+           highestBidder = msg.sender;
+           highestBid = msg.value;
+}
+
+
+function withdraw() public returns (bool) {
+        uint amount = pendingReturns[msg.sender];
+        if (amount > 0) {
+            // It is important to set this to zero because the recipient
+            // can call this function again as part of the receiving call
+            // before `send` returns.
+            pendingReturns[msg.sender] = 0;
+
+            if (!msg.sender.send(amount)) {
+                // No need to call throw here, just reset the amount owing
+                pendingReturns[msg.sender] = amount;
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /// End the auction and send the highest bid
+    /// to the beneficiary.
+
+event AuctionEnded (
+  address highestBidder,
+  uint highestBid
+  );
+
+    address previousOwner
+    function auctionEnd() public {
+        // It is a good guideline to structure functions that interact
+        // with other contracts (i.e. they call functions or send Ether)
+        // into three phases:
+        // 1. checking conditions
+        // 2. performing actions (potentially changing conditions)
+        // 3. interacting with other contracts
+        // If these phases are mixed up, the other contract could call
+        // back into the current contract and modify the state or cause
+        // effects (ether payout) to be performed multiple times.
+        // If functions called internally include interaction with external
+        // contracts, they also have to be considered interaction with
+        // external contracts.
+
+        // 1. Conditions
+        require(now >= auctionEnd, "Auction not yet ended.");
+        require(state !== "sold", "auctionEnd has already been called.");
+
+        // 2. Effects
+        state = "sold";
+        previousOwner = owner;
+        owner = highestBidder;
+        emit AuctionEnded(highestBidder, highestBid);
+
+        // 3. Interaction
+        beneficiary.transfer(highestBid);
+    }
 }
